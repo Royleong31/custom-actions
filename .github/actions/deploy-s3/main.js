7b1f72ac707dfc6dbd55a9e05fa473a7bbc3f74a
Roy Leong <l3ongroy@gmail.com>
2024-06-02 18:12:32 +0800

    add main.js

---
+++ .github/actions/deploy-s3/main.js
@@ -0,0 +1,9 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+const github = require("@actions/github");
+
+function main() {
+  console.log('hello world from main');
+}
+
+main();
