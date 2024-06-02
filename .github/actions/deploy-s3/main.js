9cbe9469d8342509e851ebade39fba31c86f9f28
Roy Leong <l3ongroy@gmail.com>
2024-06-02 23:59:58 +0800

    add output

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -10,6 +10,9 @@ function main() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   core.notice("Hello from my custom Javascript action");
+  const websiteUrl = `https://${bucket}.s3.${bucketRegion}.amazonaws.com/index.html`;
+  console.log('website url', websiteUrl);
+  core.setOutput("website-url", websiteUrl);
 }
 
 main();
